0ac954e8ba5d56f9e9cfe1b2d696b8625eeff77e
Maksim Koltsov <tirinox@gmail.com>
2020-09-16 12:15:10 +0300

    tests + download midgard transactions

--- foo.py
+++ foo.py
@@ -1,22 +1,31 @@
 import asyncio
+import aiohttp
+import json
 
-from config import Config, DB
-from fetcher import InfoFetcher, ThorInfo
+
+URL = lambda off, n: f"https://chaosnet-midgard.bepswap.com/v1/txs?offset={off}&limit={n}"
+PLACE = lambda key: f'./datavar/transactions/{key}.json'
+BATCH = 50
+
 
 
 async def main():
-    db = DB()
-    await db.get_redis()
+    async with aiohttp.ClientSession() as session:
+        count = await get_piece(0, BATCH, session)
+        print(f'total count = {count}')
+        for i in range(BATCH, count + 1, BATCH):
+            await get_piece(i, BATCH, session)
 
-    print(ThorInfo.from_json("43"))
 
-    cfg = Config()
-    fetcher = InfoFetcher(cfg)
-    j = (await fetcher.fetch_caps())
+async def get_piece(i, n, session):
+    url = URL(i, n)
+    print(f'getting {url}...')
+    async with session.get(url) as resp:
+        j = await resp.json()
+        with open(PLACE(f"{i}_{n}"), "w") as f:
+            json.dump(j, f, indent=4)
+        return int(j['count'])
 
-    print(j.as_json)
-    i = ThorInfo.from_json(j.as_json)
-    print(i)
 
-asyncio.run(main())
-
+if __name__ == '__main__':
+    asyncio.run(main())
