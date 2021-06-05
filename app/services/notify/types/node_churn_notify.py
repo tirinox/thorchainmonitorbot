d1f5b0b62175a55841df783c6a353a5863c5925f
Tirinox <developer@tirinox.ru>
2021-06-05 21:56:18 +0300

    Node Notification + add Node Diversity Pic

--- app/services/notify/types/node_churn_notify.py
+++ app/services/notify/types/node_churn_notify.py
@@ -1,10 +1,14 @@
 import logging
 
 from localization import BaseLocalization
+from services.dialog.picture.node_geo_picture import node_geo_pic
 from services.jobs.fetch.base import INotified
+from services.jobs.fetch.node_info import NodeInfoFetcher
 from services.lib.cooldown import Cooldown
 from services.lib.date_utils import MINUTE
 from services.lib.depcont import DepContainer
+from services.lib.draw_utils import img_to_bio
+from services.lib.texts import BoardMessage
 from services.models.node_info import NodeInfoChanges
 
 
@@ -17,10 +21,25 @@ class NodeChurnNotifier(INotified):
     async def on_data(self, sender, data: NodeInfoChanges):
         if not data.is_empty:
             if await self.cd.can_do():
-                await self._notify_when_cap_changed(data)
+                await self._notify_when_node_churn(data)
                 await self.cd.do()
 
-    async def _notify_when_cap_changed(self, changes: NodeInfoChanges):
+    async def _notify_when_node_churn(self, changes: NodeInfoChanges):
+        # TEXT
         await self.deps.broadcaster.notify_preconfigured_channels(self.deps.loc_man,
                                                                   BaseLocalization.notification_text_for_node_churn,
                                                                   changes)
+
+        # PICTURE
+        user_lang_map = self.deps.broadcaster.telegram_chats_from_config(self.deps.loc_man)
+
+        node_fetcher = NodeInfoFetcher(self.deps)
+        result_network_info = await node_fetcher.get_node_list_and_geo_info(node_list=changes.nodes_all)
+
+        async def node_div_pic_gen(chat_id):
+            loc: BaseLocalization = user_lang_map[chat_id]
+            graph = await node_geo_pic(result_network_info, loc)
+            bio_graph = img_to_bio(graph, "node_diversity.png")
+            return BoardMessage.make_photo(bio_graph)
+
+        await self.deps.broadcaster.broadcast(user_lang_map, node_div_pic_gen)
