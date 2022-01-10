099278ee6ae562f945a10dc93d5c199c7f7dd41a
Tirinox <developer@tirinox.ru>
2022-01-11 00:44:35 +0300

    WIP: CEX flow

--- app/services/models/bep2.py
+++ app/services/models/bep2.py
@@ -7,3 +7,12 @@ class BEP2Transfer(NamedTuple):
     block: int
     tx_hash: str
     amount: float
+
+
+class BEP2CEXFlow(NamedTuple):
+    rune_cex_inflow: float
+    rune_cex_outflow: float
+
+    @property
+    def rune_cex_netflow(self):
+        return self.rune_cex_inflow - self.rune_cex_outflow
--- app/services/notify/types/bep2_notify.py
+++ app/services/notify/types/bep2_notify.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from localization import BaseLocalization
 from services.jobs.fetch.base import INotified
 from services.lib.cooldown import Cooldown
@@ -11,16 +13,26 @@ class BEP2MoveNotifier(INotified):
     def __init__(self, deps: DepContainer):
         self.deps = deps
         self.logger = class_logger(self)
-        cd_sec = parse_timespan_to_seconds(deps.cfg.as_str('bep2.cooldown', 1))
+        cfg = deps.cfg.get('bep2', {})
+
+        cd_sec = parse_timespan_to_seconds(cfg.as_str('cooldown', 1))
         self.cd = Cooldown(self.deps.db, 'BEP2Move', cd_sec, max_times=5)
-        self.min_usd = deps.cfg.as_float('bep2.min_usd', 1000)
-        print('')
+        self.min_usd = cfg.as_float('min_usd', 1000)
+        self.cex_list = cfg.as_list('cex_list')
 
     async def on_data(self, sender, transfer: BEP2Transfer):
         rune_price = self.deps.price_holder.usd_per_rune
+
+        asyncio.create_task(self._store_transfer(transfer))
 
         if transfer.amount * rune_price >= self.min_usd:
             if await self.cd.can_do():
                 await self.deps.broadcaster.notify_preconfigured_channels(
                     BaseLocalization.notification_text_bep2_movement,
                     transfer, rune_price)
+
+    def is_cex(self, addr):
+        return addr in self.cex_list
+
+    async def _store_transfer(self, transfer: BEP2Transfer):
+        ...
