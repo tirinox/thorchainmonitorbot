c40fe4c3c726ec87c83ac1237aca374a4b45ce9c
Tirinox <developer@tirinox.ru>
2022-01-17 23:23:59 +0300

    Settings API

---
+++ app/services/lib/nop_links.py
@@ -0,0 +1,83 @@
+import ujson
+
+from services.lib.config import Config
+from services.lib.db import DB
+from services.lib.db_one2one import OneToOne
+from services.lib.utils import class_logger, random_hex
+
+
+class SettingsManager:
+    TOKEN_LEN = 16
+
+    def __init__(self, db: DB, cfg: Config):
+        self.db = db
+        self.cfg = cfg
+        self.public_url = cfg.as_str('web.public_url').rstrip('/')
+        self.logger = class_logger(self)
+        self.token_channel_db = OneToOne(db, 'Token-Channel')
+
+    def get_link(self, token):
+        return f'{self.public_url}/nodeop-setup/{token}'
+
+    @staticmethod
+    def db_key_settings(channel_id):
+        return f'Settings:Data:{channel_id}'
+
+    async def generate_new_token(self, channel_id: str):
+        await self.revoke_token(channel_id)
+        token = random_hex(self.TOKEN_LEN).decode()
+        await self.token_channel_db.put(channel_id, token)
+        return token
+
+    async def revoke_token(self, channel_id: str):
+        await self.token_channel_db.delete(channel_id)
+
+    async def get_settings(self, channel_id: str):
+        if not channel_id:
+            return {}
+        data = await self.db.redis.get(self.db_key_settings(channel_id))
+        return ujson.loads(data) if data else {}
+
+    async def get_settings_from_token(self, token: str):
+        channel = await self.token_channel_db.get(token)
+        return await self.get_settings(channel)
+
+    async def set_settings(self, channel_id, settings):
+        if not channel_id:
+            return
+        if not settings:
+            await self.db.redis.delete(self.db_key_settings(channel_id))
+        else:
+            await self.db.redis.set(self.db_key_settings(channel_id), ujson.dumps(settings))
+
+    def get_context(self, user_id):
+        return SettingsContext(self, user_id)
+
+
+class SettingsContext:
+    def __init__(self, manager: SettingsManager, user_id):
+        self.manager = manager
+        self.user_id = user_id
+        self._curr_settings = {}
+
+    async def __aenter__(self):
+        self._curr_settings = await self.manager.get_settings(self.user_id)
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.manager.set_settings(self.user_id, self._curr_settings)
+
+    def __setitem__(self, key, item):
+        self._curr_settings[key] = item
+
+    def __getitem__(self, key):
+        return self._curr_settings[key]
+
+    def __repr__(self):
+        return repr(self._curr_settings)
+
+    def __len__(self):
+        return len(self._curr_settings)
+
+    def __delitem__(self, key):
+        del self._curr_settings[key]
--- app/web_api.py
+++ app/web_api.py
@@ -1,9 +1,15 @@
 import asyncio
 import logging
 
+import uvicorn
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
 from services.lib.config import Config
 from services.lib.db import DB
 from services.lib.depcont import DepContainer
+from services.lib.nop_links import SettingsManager
 from services.lib.utils import setup_logs
 
 
@@ -12,8 +18,8 @@ class AppSettingsAPI:
         d = self.deps = DepContainer()
         d.cfg = Config()
 
-        log_level = d.cfg.get_pure('log_level', logging.INFO)
-        setup_logs(log_level)
+        self.log_level = d.cfg.get_pure('log_level', logging.INFO)
+        setup_logs(self.log_level)
 
         logging.info(f'Starting Web API for THORChain monitoring bot @ "{d.cfg.network_id}".')
 
@@ -22,8 +28,70 @@ class AppSettingsAPI:
         d.loop = asyncio.get_event_loop()
         d.db = DB(d.loop)
 
+        self.web_app = Starlette(
+            debug=bool(d.cfg.web.debug),
+            routes=self._routes(),
+            on_startup=[self._on_startup]
+        )
+        self.manager = SettingsManager(d.db, d.cfg)
+
+    async def _on_startup(self):
+        await self.deps.db.get_redis()
+        # print(await self.manager.generate_new_token('test_c'))  # debug
+
+    async def _get_settings(self, request):
+        token = request.path_params.get('token')
+        channel_id = await self.manager.token_channel_db.get(token)
+        if not channel_id:
+            return JSONResponse({
+                'error': 'channel not found'
+            })
+        settings = await self.manager.get_settings(channel_id)
+        return JSONResponse({
+            'channel': channel_id,
+            'settings': settings,
+        })
+
+    async def _set_settings(self, request):
+        token = request.path_params.get('token')
+        channel_id = await self.manager.token_channel_db.get(token)
+        if not channel_id:
+            return JSONResponse({
+                'error': 'channel not found'
+            })
+        data = await request.json()
+        await self.manager.set_settings(channel_id, data)
+        return JSONResponse({'channel': channel_id, 'data': data})
+
+    async def _del_settings(self, request):
+        token = request.path_params.get('token')
+        channel_id = await self.manager.token_channel_db.get(token)
+        if not channel_id:
+            return JSONResponse({
+                'error': 'channel not found'
+            })
+        await self.manager.revoke_token(channel_id)
+        return JSONResponse({'channel': channel_id, 'status': 'revoked'})
+
+    def _routes(self):
+        return [
+            Route('/settings/{token}', self._get_settings, methods=['GET']),
+            Route('/settings/{token}', self._set_settings, methods=['POST']),
+            Route('/settings/{token}', self._del_settings, methods=['DELETE'])
+        ]
+
     def run(self):
-        ...
+        cfg = self.deps.cfg.web
+        port = int(cfg.get('port', 8000))
+        host = str(cfg.get('host', 'localhost'))
+        # reload = bool(cfg.get('reload', False))
+        uvicorn.run(
+            self.web_app, http='h11',
+            loop='asyncio', port=port,
+            # reload=reload,
+            log_level=self.log_level.lower(),
+            host=host
+        )
 
 
 if __name__ == '__main__':
