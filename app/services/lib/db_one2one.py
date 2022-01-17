c4de207dcbb36d0514d982a733e11ddd57515fcf
Tirinox <developer@tirinox.ru>
2022-01-17 22:05:17 +0300

    DB: one2one Class

---
+++ app/services/lib/db_one2one.py
@@ -0,0 +1,40 @@
+import asyncio
+
+from aioredis import Redis
+
+from services.lib.db import DB
+
+
+class OneToOne:
+    def __init__(self, db: DB, prefix: str):
+        self.db = db
+        self.prefix = prefix
+
+    async def _redis(self) -> Redis:
+        return await self.db.get_redis()
+
+    def key(self, k):
+        return f'121:{self.prefix}:{k}'
+
+    async def clear(self):
+        r = await self._redis()
+        keys = await r.keys(self.key('*'))
+        if keys:
+            await r.delete(*keys)
+
+    async def put(self, one, two, safe=True):
+        r = self.db.redis
+        if safe:
+            await self.delete(one)
+        await asyncio.gather(
+            r.set(self.key(one), two),
+            r.set(self.key(two), one)
+        )
+
+    async def get(self, k):
+        return await self.db.redis.get(self.key(k))
+
+    async def delete(self, name):
+        partner = await self.get(name)
+        items = (partner, name) if partner else (name,)
+        await self.db.redis.delete(*map(self.key, items))
---
+++ app/tests/test_121.py
@@ -0,0 +1,75 @@
+import asyncio
+
+import pytest
+
+from services.lib.db import DB
+from services.lib.db_one2one import OneToOne
+
+
+@pytest.fixture(scope="function")
+def one2one():
+    loop = asyncio.get_event_loop()
+    db = DB(loop)
+    return OneToOne(db, 'Test')
+
+
+@pytest.mark.asyncio
+async def test_simple(one2one: OneToOne):
+    await one2one.clear()
+    await one2one.put('1', '2')
+    assert await one2one.get('1') == '2'
+    assert await one2one.get('2') == '1'
+    assert await one2one.get(1) == '2'
+    assert await one2one.get(2) == '1'
+
+    assert not await one2one.get('3')
+    assert not await one2one.get(None)
+
+    await one2one.delete('1')
+    assert not await one2one.get('1')
+    assert not await one2one.get('2')
+
+
+@pytest.mark.asyncio
+async def test_same_name(one2one: OneToOne):
+    await one2one.clear()
+    await one2one.put('foo', 'foo')
+    assert await one2one.get('foo') == 'foo'
+    await one2one.delete('foo')
+    assert await one2one.get('foo') is None
+
+
+@pytest.mark.asyncio
+async def test_clear(one2one: OneToOne):
+    await one2one.clear()
+    await one2one.put('1', '2')
+    await one2one.put('3', '4')
+    await one2one.put('5', '6')
+    assert await one2one.get('6') == '5'
+
+    await one2one.clear()
+    assert not await one2one.get('1')
+    assert not await one2one.get('2')
+    assert not await one2one.get('3')
+    assert not await one2one.get('4')
+    assert not await one2one.get('5')
+    assert not await one2one.get('6')
+
+
+@pytest.mark.asyncio
+async def test_confuse(one2one: OneToOne):
+    await one2one.clear()
+    await one2one.put('me', 'good')
+    await one2one.put('good', 'evil')
+
+    assert not await one2one.get('me')
+    assert await one2one.get('good') == 'evil'
+    assert await one2one.get('evil') == 'good'
+
+    await one2one.clear()
+    await one2one.put('me', '123')
+    await one2one.put('me', '345')
+
+    assert not await one2one.get('123')
+    assert await one2one.get('me') == '345'
+    assert await one2one.get('345') == 'me'
