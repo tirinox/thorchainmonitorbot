7dada42a18b3a49f7c90edf725afd060b63ae66b
Tirinox <developer@tirinox.ru>
2022-11-11 23:20:55 +0300

    BaseFetcher: run_once

--- app/services/jobs/fetch/base.py
+++ app/services/jobs/fetch/base.py
@@ -22,6 +22,18 @@ class BaseFetcher(WithDelegates, ABC):
     async def fetch(self):
         ...
 
+    async def run_once(self):
+        try:
+            data = await self.fetch()
+            await self.pass_data_to_listeners(data)
+            await self.post_action(data)
+        except Exception as e:
+            self.logger.exception(f"task error: {e}")
+            try:
+                await self.handle_error(e)
+            except Exception as e:
+                self.logger.exception(f"task error while handling on_error: {e}")
+
     async def run(self):
         if self.sleep_period < 0:
             self.logger.info('This fetcher is disabled.')
@@ -29,16 +41,5 @@ class BaseFetcher(WithDelegates, ABC):
 
         await asyncio.sleep(self.initial_sleep)
         while True:
-            try:
-                data = await self.fetch()
-                await self.pass_data_to_listeners(data)
-                await self.post_action(data)
-            except Exception as e:
-                self.logger.exception(f"task error: {e}")
-
-                try:
-                    await self.handle_error(e)
-                except Exception as e:
-                    self.logger.exception(f"task error while handling on_error: {e}")
-
+            await self.run_once()
             await asyncio.sleep(self.sleep_period)
