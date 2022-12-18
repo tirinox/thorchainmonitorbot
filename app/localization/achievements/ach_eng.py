31fa436df0fc823dfcbca86b966a81181e4e0d42
Tirinox <developer@tirinox.ru>
2022-12-18 22:30:35 +0300

    More ach descriptions (not final) + fill test

--- app/localization/achievements/ach_eng.py
+++ app/localization/achievements/ach_eng.py
@@ -17,12 +17,40 @@ class AchievementDescription(NamedTuple):
         return f'ach_{self.key}.png'
 
 
-LIST = [
+ACHIEVEMENT_DESC_LIST = [
+    AchievementDescription(Achievement.TEST, 'Test achievement'),
     AchievementDescription(Achievement.DAU, 'Daily active users'),
     AchievementDescription(Achievement.MAU, 'Monthly active users'),
     AchievementDescription(Achievement.WALLET_COUNT, 'Wallets count'),
-    AchievementDescription(Achievement.TEST, 'Test achievement'),
+    AchievementDescription(Achievement.SWAP_COUNT_TOTAL, 'Total swaps count'),
+    AchievementDescription(Achievement.SWAP_COUNT_24H, '24h swaps count'),
+    AchievementDescription(Achievement.SWAP_COUNT_30D, 'Monthly swap count'),
+    AchievementDescription(Achievement.SWAP_UNIQUE_COUNT, 'Unique swappers'),
+    AchievementDescription(Achievement.ADD_LIQUIDITY_COUNT_TOTAL, 'Total add liquidity count'),
+    AchievementDescription(Achievement.ADD_LIQUIDITY_VOLUME_TOTAL, 'Total add liquidity volume'),
+    AchievementDescription(Achievement.DAILY_VOLUME, 'Daily volume'),
+    AchievementDescription(Achievement.ILP_PAID_TOTAL, 'Total ILP paid'),
+    AchievementDescription(Achievement.TOTAL_ACTIVE_BOND, 'Total active bond'),
+    AchievementDescription(Achievement.TOTAL_BOND, 'Total bond'),
+    AchievementDescription(Achievement.NODE_COUNT, 'Total nodes count'),
+    AchievementDescription(Achievement.ACTIVE_NODE_COUNT, 'Active nodes count'),
+    AchievementDescription(Achievement.CHURNED_IN_BOND, 'Churned in bond'),
+    AchievementDescription(Achievement.ANNIVERSARY, 'Anniversary'),
+    AchievementDescription(Achievement.BLOCK_NUMBER, 'Blocks generated'),
+    AchievementDescription(Achievement.DAILY_TX_COUNT, 'Daily TX count'),
 ]
+
+ACHIEVEMENT_DESC_MAP = {a.key: a for a in ACHIEVEMENT_DESC_LIST}
+
+
+def check_if_all_achievements_have_description():
+    all_achievements = set(Achievement.all_keys())
+    all_achievements_with_desc = set(ACHIEVEMENT_DESC_MAP.keys())
+    assert all_achievements == all_achievements_with_desc, \
+        f'Not all achievements have description. Missing: {all_achievements - all_achievements_with_desc}'
+
+
+check_if_all_achievements_have_description()
 
 
 class AchievementsEnglishLocalization:
