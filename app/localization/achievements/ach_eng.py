3b303fabfe839d0d469d7a5a1b8d4a91148f5e39
Tirinox <developer@tirinox.ru>
2022-12-19 14:11:14 +0300

    Improve AchDescs;

--- app/localization/achievements/ach_eng.py
+++ app/localization/achievements/ach_eng.py
@@ -1,7 +1,9 @@
+import random
 from typing import NamedTuple
 
 from services.jobs.achievements import EventAchievement, Achievement
 from services.lib.date_utils import seconds_human
+from services.lib.money import RAIDO_GLYPH
 from services.lib.texts import code, pre
 
 
@@ -18,7 +20,7 @@ class AchievementDescription(NamedTuple):
 
 
 ACHIEVEMENT_DESC_LIST = [
-    AchievementDescription(Achievement.TEST, 'Test achievement'),
+    AchievementDescription(Achievement.TEST, 'Test metric'),
     AchievementDescription(Achievement.DAU, 'Daily active users'),
     AchievementDescription(Achievement.MAU, 'Monthly active users'),
     AchievementDescription(Achievement.WALLET_COUNT, 'Wallets count'),
@@ -28,17 +30,20 @@ ACHIEVEMENT_DESC_LIST = [
     AchievementDescription(Achievement.SWAP_UNIQUE_COUNT, 'Unique swappers'),
     AchievementDescription(Achievement.ADD_LIQUIDITY_COUNT_TOTAL, 'Total add liquidity count'),
     AchievementDescription(Achievement.ADD_LIQUIDITY_VOLUME_TOTAL, 'Total add liquidity volume'),
-    AchievementDescription(Achievement.DAILY_VOLUME, 'Daily volume'),
-    AchievementDescription(Achievement.ILP_PAID_TOTAL, 'Total ILP paid'),
+    AchievementDescription(Achievement.DAILY_VOLUME, 'Daily volume', prefix='$'),
+    AchievementDescription(Achievement.ILP_PAID_TOTAL, 'Total ILP paid', postfix=RAIDO_GLYPH),
     AchievementDescription(Achievement.TOTAL_ACTIVE_BOND, 'Total active bond'),
-    AchievementDescription(Achievement.TOTAL_BOND, 'Total bond'),
-    AchievementDescription(Achievement.NODE_COUNT, 'Total nodes count'),
+    AchievementDescription(Achievement.TOTAL_BOND, 'Total bond', postfix=RAIDO_GLYPH),
+    AchievementDescription(Achievement.NODE_COUNT, 'Total nodes count', postfix=RAIDO_GLYPH),
     AchievementDescription(Achievement.ACTIVE_NODE_COUNT, 'Active nodes count'),
-    AchievementDescription(Achievement.CHURNED_IN_BOND, 'Churned in bond'),
+    AchievementDescription(Achievement.CHURNED_IN_BOND, 'Churned in bond', postfix=RAIDO_GLYPH),
     AchievementDescription(Achievement.ANNIVERSARY, 'Anniversary'),
     AchievementDescription(Achievement.BLOCK_NUMBER, 'Blocks generated'),
     AchievementDescription(Achievement.DAILY_TX_COUNT, 'Daily TX count'),
     AchievementDescription(Achievement.TOTAL_MIMIR_VOTES, 'Total Mimir votes'),
+    AchievementDescription(Achievement.MARKET_CAP_USD, 'Market cap USD', prefix='$'),
+    AchievementDescription(Achievement.TOTAL_POOLS, 'Total pools'),
+    AchievementDescription(Achievement.TOTAL_ACTIVE_POOLS, 'Active pools'),
 ]
 
 ACHIEVEMENT_DESC_MAP = {a.key: a for a in ACHIEVEMENT_DESC_LIST}
@@ -55,9 +60,18 @@ check_if_all_achievements_have_description()
 
 
 class AchievementsEnglishLocalization:
+    CELEBRATION_EMOJIES = "🎉🎊🥳🙌🥂🪅🎆"
+
     @staticmethod
     def get_achievement_description(achievement: str) -> AchievementDescription:
         return ACHIEVEMENT_DESC_MAP[achievement]
+
+    @staticmethod
+    def format_value(achievement: str, value: str) -> str:
+        # todo!
+        if achievement == Achievement.ANNIVERSARY:
+            return seconds_human(int(value))
+        return value
 
     @classmethod
     def notification_achievement_unlocked(cls, e: EventAchievement):
@@ -65,7 +79,10 @@ class AchievementsEnglishLocalization:
 
         desc = cls.get_achievement_description(a.key)
 
+        emoji = random.choice(cls.CELEBRATION_EMOJIES)
         ago = seconds_human(a.timestamp - a.previous_ts)
-        return f'<b>You have unlocked achievement "{a.key}"!</b> ' \
-               f'{code(a.key)} is over {a.milestone} ({pre(a.value)})\n ' \
-               f'(previous {pre(a.prev_milestone)} {ago} ago)'
+        return (
+            f'{emoji} <b>A new achievement has been unlocked</b>\n'
+            f'{pre(desc)} is now over {code(a.milestone)} ({pre(a.value)})!\n '
+            f'(Previously {pre(a.prev_milestone)} {ago} ago)'
+        )
