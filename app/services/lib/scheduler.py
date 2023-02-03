da248dbff05fb2704a81420bc78b92ad493908ff
Tirinox <developer@tirinox.ru>
2023-02-03 19:17:16 +0300

    Scheduler: cancel by ident and forget_after

--- app/services/lib/scheduler.py
+++ app/services/lib/scheduler.py
@@ -2,31 +2,33 @@ import asyncio
 
 from aioredis import Redis
 
-from services.lib.date_utils import now_ts
+from services.lib.date_utils import now_ts, DAY
 from services.lib.delegates import WithDelegates
 from services.lib.utils import WithLogger
 
 
 class Scheduler(WithLogger, WithDelegates):
-    def __init__(self, r: Redis, name, poll_interval: float = 10):
+    def __init__(self, r: Redis, name, poll_interval: float = 10, forget_after=DAY):
         assert name
         super().__init__()
         self.name = name
         self._poll_interval = poll_interval
         self._r = r
         self._running = False
+        self.forget_after = forget_after
 
-    async def schedule(self, ident, timestamp=0, period=0.0):
+    async def schedule(self, ident, timestamp=0.0, period=0.0):
         assert isinstance(ident, (str, int, float)) and ident, 'ident must be a string or number'
 
+        ev_desc = self.ev_desc(ident)
         now = now_ts()
         if not timestamp:
             timestamp = now
         elif timestamp < now:
-            self.logger.warning(f'Scheduling: {self.name}:{ident} at {timestamp} (before now!)')
+            self.logger.warning(f'Scheduling: {ev_desc} at {timestamp} (before now!)')
             return
 
-        self.logger.debug(f'Scheduling: {self.name}:{ident} at {timestamp} ({timestamp - now} seconds from now)')
+        self.logger.debug(f'Scheduling: {ev_desc} at {timestamp} ({timestamp - now} seconds from now)')
 
         await self._r.zadd(self.key_timeline(), {ident: timestamp})
 
@@ -36,14 +38,21 @@ class Scheduler(WithLogger, WithDelegates):
         else:
             await self._r.delete(key_period)
 
-    async def _run_handler(self, name, ev):
+    def ev_desc(self, ident):
+        return f'"{self.name}:{ident}"'
+
+    async def _run_handler(self, ev):
         try:
             now = now_ts()
             delay = now - ev[1]
-            self.logger.debug(f'Running scheduler handler: {name}, delay: {delay:.3f} sec')
             ident = ev[0]
-            await self.pass_data_to_listeners(ident)
-            self.logger.debug(f'Finished scheduler handler: {name}')
+            ev_desc = self.ev_desc(ident)
+
+            if 0 < self.forget_after < delay:
+                self.logger.info(f'Event seems forgotten: {ev_desc}. Ignoring.')
+                return
+
+            self.logger.debug(f'Running scheduler handler: {ev_desc}, delay: {delay:.3f} sec')
 
             period = await self._r.get(self.key_period(ident))
             if period:
@@ -51,8 +60,14 @@ class Scheduler(WithLogger, WithDelegates):
                     period = float(period)
                     if period > 0:
                         await self.schedule(ident, now + period, period)
+                    elif period < 0:
+                        self.logger.info(f'Periodic event seems cancelled: {ev_desc}. Ignoring.')
+                        return
                 except ValueError:
-                    self.logger.warning(f'Invalid period: {period}. Failed to reschedule: {name}:{ident}')
+                    self.logger.warning(f'Invalid period: {period}. Failed to reschedule: {ev_desc}')
+
+            await self.pass_data_to_listeners(ident)
+            self.logger.debug(f'Finished scheduler handler: {ev_desc}')
 
         except Exception as e:
             self.logger.exception(f'Error in scheduler handler: {e}', stack_info=True)
@@ -60,16 +75,18 @@ class Scheduler(WithLogger, WithDelegates):
     async def awaiting_events(self):
         return await self._r.zrange(self.key_timeline(), 0, -1, withscores=True)
 
-    async def all_periodic_events(self):
-        return await self._r.keys(self.key_period('*'))
+    async def all_periodic_events(self, ident=None):
+        return await self._r.keys(self.key_period(ident or '*'))
 
     async def cancel(self, ident):
         await self._r.zrem(self.key_timeline(), ident)
         await self._r.delete(self.key_period(ident))
+        self.logger.debug(f'Cancelled: {self.ev_desc(ident)}')
 
-    async def cancel_all_periodic(self):
-        for key in await self.all_periodic_events():
-            await self._r.delete(key)
+    async def cancel_all_periodic(self, ident=None):
+        keys = await self.all_periodic_events(ident)
+        if keys:
+            await self._r.delete(*keys)
 
     async def _process(self):
         now = now_ts()
@@ -77,7 +94,7 @@ class Scheduler(WithLogger, WithDelegates):
         evs = await self._r.zrangebyscore(key_timeline, 0, now, withscores=True)
         await self._r.zremrangebyscore(key_timeline, 0, now)
         for ev in evs:
-            await self._run_handler(self.name, ev)
+            await self._run_handler(ev)
 
     def key_timeline(self):
         return f'Scheduler:{self.name}:TimeLine'
--- app/tools/debug/dbg_sched.py
+++ app/tools/debug/dbg_sched.py
@@ -19,9 +19,32 @@ async def demo_run_scheduler_simple_1(sched: Scheduler):
 
 
 async def demo_run_scheduler_simple_2_per(sched: Scheduler):
-    # fixme: data is a key!! key must be unique. store period and other data somewhere else
     await sched.schedule('1', now_ts() + 1.0, period=5)
     await sched.schedule('2', now_ts() + 1.5, period=5.1)
+    await sched.run()
+
+
+async def demo_cancel_periodic_3(sched: Scheduler):
+    await sched.schedule('Troll', period=1)
+
+    async def cancel_guy():
+        await asyncio.sleep(3)
+        await sched.cancel('Troll')
+
+    asyncio.create_task(cancel_guy())  # in background
+    await sched.run()
+
+
+async def demo_ignore_old_4(sched: Scheduler):
+    sched.forget_after = 1
+
+    await sched.schedule('Old', now_ts() + 2)
+    await sched.schedule('WillFire', now_ts() + 5)
+
+    print('Sleeping 4 seconds...')
+    await asyncio.sleep(4)
+
+    print('Run!')
     await sched.run()
 
 
@@ -34,8 +57,10 @@ async def run():
 
         await sched.cancel_all_periodic()
 
-        await demo_run_scheduler_simple_1(sched)
+        # await demo_run_scheduler_simple_1(sched)
         # await demo_run_scheduler_simple_2_per(sched)
+        # await demo_cancel_periodic_3(sched)
+        await demo_ignore_old_4(sched)
 
 
 if __name__ == '__main__':
