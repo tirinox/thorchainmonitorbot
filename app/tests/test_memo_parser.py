d7e688613ba4ff91c30c38f91ace61048eab1327
Tirinox <developer@tirinox.ru>
2023-07-23 11:57:55 +0300

    Memo parser + streaming swaps + unit-test

--- app/tests/test_memo_parser.py
+++ app/tests/test_memo_parser.py
@@ -3,16 +3,39 @@ from services.models.tx import ThorTxType
 
 
 def test_memo1():
-    memo = \
-        '=:ETH.ETH:0xA58818F1cA5A7DD524Eca1F89E2325e15BAD6cc4::::FC4414199:0xd533a949740bb3306d119cc777fa900ba034cd52'
-    m = THORMemoParsed.parse_memo(memo)
+    m = THORMemoParsed.parse_memo('=:ETH.ETH:0xA58818F1cA5A7DD524Eca1F89E2325e15BAD6cc4:'
+                                  ':'
+                                  ':'
+                                  ':FC4414199:0xd533a949740bb3306d119cc777fa900ba034cd52')
     assert m.action == ThorTxType.TYPE_SWAP
     assert m.asset == 'ETH.ETH'
     assert m.dex_aggregator_address == 'FC4414199'
     assert m.limit == 0
     assert m.final_asset_address == '0xd533a949740bb3306d119cc777fa900ba034cd52'
     assert m.affiliate_fee == 0
-    assert m.affiliate_address == None
+    assert m.affiliate_address is None
+    assert m.s_swap_quantity == 0
+    assert m.s_swap_interval == 0
+
+    m = THORMemoParsed.parse_memo('SWAP:BTC/BTC:thorname')
+    assert m.action == ThorTxType.TYPE_SWAP
+    assert m.asset == 'BTC/BTC'
+    assert m.dex_aggregator_address is None
+    assert m.final_asset_address is None
+    assert m.dest_address == 'thorname'
+    assert m.limit == 0
+    assert m.s_swap_quantity == 0
+    assert m.s_swap_interval == 0
+
+    m = THORMemoParsed.parse_memo('SWAP:AVAX.AVAX:0x12345678901234589012345:18000000/5/20:t:50')
+    assert m.action == ThorTxType.TYPE_SWAP
+    assert m.asset == 'AVAX.AVAX'
+    assert m.dest_address == '0x12345678901234589012345'
+    assert m.limit == 18000000
+    assert m.s_swap_interval == 5
+    assert m.s_swap_quantity == 20
+    assert m.affiliate_address == 't'
+    assert m.affiliate_fee == 0.005
 
 
 def test_memo2():
