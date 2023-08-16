0b9ac2e2fb9e646ef5579d664e1e12451a303fa5
Tirinox <developer@tirinox.ru>
2023-08-16 20:53:38 +0300

    Utils: say(text)

--- app/services/lib/utils.py
+++ app/services/lib/utils.py
@@ -491,3 +491,17 @@ def recursive_asdict(j):
 
 def strip_trailing_slash(s: str):
     return s.rstrip('/')
+
+
+async def say(msg: str):
+    if msg:
+        msg = msg.replace('"', '').replace('\\b', '')
+
+        def worker():
+            os.system('afplay /System/Library/Sounds/Sosumi.aiff')
+            os.system(f'say "{msg}"')
+
+        async def a_worker():
+            await asyncio.get_event_loop().run_in_executor(None, worker)
+
+        asyncio.create_task(a_worker())
