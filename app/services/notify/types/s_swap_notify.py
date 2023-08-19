c4eaab4f5c2eae5427870c8b77cfb18abda92c68
Tirinox <developer@tirinox.ru>
2023-08-19 08:38:05 +0300

    Streaming swap started: minimul threshold USD volume

--- app/services/notify/types/s_swap_notify.py
+++ app/services/notify/types/s_swap_notify.py
@@ -13,11 +13,17 @@ class StreamingSwapStartTxNotifier(INotified, WithDelegates, WithLogger):
         self.prefix = prefix
         self.detector = SwapStartDetector(deps)
         self._ev_db = EventDatabase(deps.db)
+        self.min_streaming_swap_usd = self.deps.cfg.as_float(
+            'tx.swap.also_trigger_when.streaming_swap.volume_greater', 2500.0)
 
     async def on_data(self, sender, data: BlockResult):
+        if self.min_streaming_swap_usd <= 0.0:
+            return
+
         swaps = self.detector.detect_swaps(data)
         for swap_start_ev in swaps:
             if swap_start_ev.is_streaming:
-                if not await self._ev_db.is_announced_as_started(tx_id := swap_start_ev.tx_id):
-                    await self.pass_data_to_listeners(swap_start_ev)
-                    await self._ev_db.announce_tx_started(tx_id)
+                if swap_start_ev.volume_usd >= self.min_streaming_swap_usd:
+                    if not await self._ev_db.is_announced_as_started(tx_id := swap_start_ev.tx_id):
+                        await self._ev_db.announce_tx_started(tx_id)
+                        await self.pass_data_to_listeners(swap_start_ev)
