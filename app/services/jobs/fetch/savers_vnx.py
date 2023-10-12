5943d8cf8a587b0a7a1b9673a163eda908529e07
Tirinox <developer@tirinox.ru>
2023-10-12 23:02:52 +0300

    Savers vaults: fix after API has changed.

--- app/services/jobs/fetch/savers_vnx.py
+++ app/services/jobs/fetch/savers_vnx.py
@@ -1,4 +1,3 @@
-import asyncio
 from contextlib import suppress
 from typing import NamedTuple, List
 
@@ -15,27 +14,42 @@ class VNXSaversStats(NamedTuple):
     asset: str
     asset_depth: int
     asset_price: float
-    delta_earned: float
     earned: float
+    earned_old: float
     filled: float
     savers_count: int
+    savers_count_old: int
     saver_return: float
+    saver_return_old: float
     savers_depth: int
+    savers_depth_old: int
     synth_supply: int
 
     @classmethod
     def from_json(cls, j):
+        new_j = j.get('savers')
+        old_j = j.get('oldSavers')
+
         return cls(
-            asset=j.get('asset'),
-            asset_depth=int(j.get('assetDepth')),
-            asset_price=float(j.get('assetPrice')),
-            delta_earned=float(j.get('deltaEarned')),
-            earned=float(j.get('earned')),
-            filled=float(j.get('filled')),
-            savers_count=int(j.get('saversCount')),
-            saver_return=float(j.get('saverReturn', 0.0) or 0.0),
-            savers_depth=int(j.get('saversDepth')),
-            synth_supply=int(j.get('synthSupply')),
+            asset=new_j.get('asset', ''),
+            asset_depth=int(new_j.get('assetDepth', 0.0)),
+            asset_price=float(new_j.get('assetPriceUSD', 0.0)),
+
+            filled=float(new_j.get('filled', 0.0)),
+
+            earned=float(new_j.get('earned', 0.0)),
+            earned_old=float(old_j.get('earned', 0.0)),
+
+            savers_count=int(new_j.get('saversCount', 0)),
+            savers_count_old=int(old_j.get('saversCount', 0)),
+
+            saver_return=float(new_j.get('saversReturn', 0.0) or 0.0),
+            saver_return_old=float(old_j.get('saversReturn', 0.0) or 0.0),
+
+            savers_depth=int(new_j.get('saversDepth', 0.0)),
+            savers_depth_old=int(old_j.get('saversDepth', 0.0)),
+
+            synth_supply=int(new_j.get('synthSupply', 0.0)),
         )
 
 
@@ -44,11 +58,14 @@ class VNXSaversStatsFetcher(INotified, WithDelegates, WithLogger):
         super().__init__()
         self.deps = deps
 
-    VNX_SAVERS_STATS_URL = 'https://vanaheimex.com/api/saversExtraData'
-    VNX_SAVERS_STATS_PREV_URL = 'https://vanaheimex.com/api/oldSaversExtraData'
+    VNX_SAVERS_STATS_URL = 'https://vanaheimex.com/api/saversInfo'
 
-    def convert(self, stats: VNXSaversStats, price_holder: LastPriceHolder):
+    def convert(self, stats: VNXSaversStats, price_holder: LastPriceHolder, new=True):
         amount = thor_to_float(stats.savers_depth)
+        amount_old = thor_to_float(stats.savers_depth_old)
+        amount_usd = stats.asset_price * amount
+        amount_usd_old = stats.asset_price * amount_old
+
         usd_per_rune = self.deps.price_holder.usd_per_rune
 
         max_synth_per_asset_ratio = self.deps.mimir_const_holder.get_max_synth_per_pool_depth()  # normally: 0.15
@@ -56,21 +73,28 @@ class VNXSaversStatsFetcher(INotified, WithDelegates, WithLogger):
         pool = price_holder.find_pool(stats.asset)
         cap = pool.get_synth_cap_in_asset_float(max_synth_per_asset_ratio)
         rune_earned = thor_to_float(stats.earned) * stats.asset_price / usd_per_rune
+        rune_earned_old = thor_to_float(stats.earned_old) * stats.asset_price / usd_per_rune
 
         return SaverVault(
             asset=stats.asset,
-            number_of_savers=stats.savers_count,
-            total_asset_saved=amount,
-            total_asset_saved_usd=stats.asset_price * amount,
-            apr=stats.saver_return * 100.0,
+            number_of_savers=stats.savers_count if new else stats.savers_count_old,
+            total_asset_saved=amount if new else amount_old,
+            total_asset_saved_usd=amount_usd if new else amount_usd_old,
+            apr=(stats.saver_return if new else stats.saver_return_old) * 100.0,
             asset_cap=cap,
-            runes_earned=rune_earned,
+            runes_earned=rune_earned if new else rune_earned_old,
             synth_supply=thor_to_float(stats.synth_supply),
         )
 
-    async def load_real_yield_vanaheimex(self, old=False) -> List[SaverVault]:
-        url = self.VNX_SAVERS_STATS_PREV_URL if old else self.VNX_SAVERS_STATS_URL
+    async def load_real_yield_vanaheimex(self) -> List[VNXSaversStats]:
+        url = self.VNX_SAVERS_STATS_URL
+        self.logger.debug(f'Loading {url!r}...')
         async with self.deps.session.get(url) as resp:
+            if resp.status != 200:
+                self.logger.error(f'Response code for {url!r} is {resp.status}!')
+                self.deps.emergency.report('VNXSaversStatsFetcher', 'Failed to load',
+                                           status=resp.status, url=url)
+
             j = await resp.json()
             if not j:
                 return []
@@ -78,8 +102,14 @@ class VNXSaversStatsFetcher(INotified, WithDelegates, WithLogger):
             results = []
             for asset, sub_json in j.items():
                 saver_vault = VNXSaversStats.from_json(sub_json)
-                saver_vault = self.convert(saver_vault, self.deps.price_holder)
                 results.append(saver_vault)
+
+            if not results:
+                self.logger.error(f'No data received')
+                self.deps.emergency.report('VNXSaversStatsFetcher', 'No data received', url=url)
+            else:
+                self.logger.info(f'Got {len(results)} rows.')
+
             return results
 
     @staticmethod
@@ -90,19 +120,12 @@ class VNXSaversStatsFetcher(INotified, WithDelegates, WithLogger):
         return SaversBank(n, vaults)
 
     async def get_savers_event(self, *args) -> AlertSaverStats:
-        savers_stats_new, savers_stats_old = await asyncio.gather(
-            self.load_real_yield_vanaheimex(),
-            self.load_real_yield_vanaheimex(old=True)
-        )
-
-        price_holder = self.deps.price_holder
-
-        curr_saver = self.make_bank(savers_stats_new)
-        prev_saver = self.make_bank(savers_stats_old)
-
-        return AlertSaverStats(
-            prev_saver, curr_saver, price_holder
-        )
+        vnx_vaults = await self.load_real_yield_vanaheimex()
+        vaults = [self.convert(v, self.deps.price_holder) for v in vnx_vaults]
+        curr_saver = self.make_bank(vaults)
+        old_vaults = [self.convert(v, self.deps.price_holder, new=False) for v in vnx_vaults]
+        prev_state = self.make_bank(old_vaults)
+        return AlertSaverStats(prev_state, curr_saver, self.deps.price_holder)
 
     CACHE_TTL = 60
 
@@ -113,5 +136,6 @@ class VNXSaversStatsFetcher(INotified, WithDelegates, WithLogger):
     async def on_data(self, sender, data):
         with suppress(Exception):
             vaults = await self.load_real_yield_vanaheimex()
+            vaults = [self.convert(v, self.deps.price_holder) for v in vaults]
             bank = self.make_bank(vaults)
             await self.pass_data_to_listeners(bank)
