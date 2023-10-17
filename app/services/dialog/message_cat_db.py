f5c2da142d186d33a27283e20bddc43f04766791
Tirinox <developer@tirinox.ru>
2023-10-17 21:04:12 +0300

    Message Category DB

---
+++ app/services/dialog/message_cat_db.py
@@ -0,0 +1,52 @@
+from aiogram.utils.exceptions import MessageCantBeDeleted, MessageToEditNotFound, MessageToDeleteNotFound
+
+from services.dialog.telegram.telegram import TelegramBot
+from services.lib.db import DB
+from services.lib.logs import WithLogger
+
+
+class MessageCategoryDB(WithLogger):
+    def __init__(self, db: DB, user_id: int, category: str):
+        super().__init__()
+        self.db = db
+        self.user_id = user_id
+        self.category = category
+
+    @property
+    def db_key(self):
+        return f'Message:Tracker:{self.user_id}:{self.category}'
+
+    async def push(self, message_id):
+        if not message_id:
+            self.logger.error(f'U: {self.user_id} Empty message_id for category {self.category}')
+            return
+
+        key = self.db_key
+        await self.db.redis.sadd(key, message_id)
+
+    async def pop(self):
+        key = self.db_key
+        return await self.db.redis.spop(key)
+
+    async def get_all(self):
+        key = self.db_key
+        return await self.db.redis.smembers(key)
+
+    async def clear(self):
+        key = self.db_key
+        await self.db.redis.delete(key)
+
+    async def pop_delete(self, bot: TelegramBot):
+        message_id = await self.pop()
+        if message_id:
+            try:
+                self.logger.info(f'U: {self.user_id} Deleting message {message_id}')
+                await bot.bot.delete_message(self.user_id, message_id)
+            except (MessageCantBeDeleted, MessageToEditNotFound, MessageToDeleteNotFound) as e:
+                self.logger.error(f'can not delete message {e!r}')
+        return message_id
+
+    async def delete_all(self, bot: TelegramBot):
+        message_id = await self.pop_delete(bot)
+        while message_id:
+            message_id = await self.pop_delete(bot)
---
+++ app/tests/test_message_cat.py
@@ -0,0 +1,38 @@
+import asyncio
+
+import pytest
+
+from services.dialog.message_cat_db import MessageCategoryDB
+from services.lib.db import DB
+
+
+@pytest.fixture(scope='session')
+async def m_cat_db():
+    loop = asyncio.get_event_loop()
+    db = DB(loop)
+    await db.get_redis()
+
+    db = MessageCategoryDB(db, 123, 'test')
+    await db.clear()
+    return db
+
+
+@pytest.mark.asyncio
+async def test_push_pop(m_cat_db):
+    m_cat_db = await m_cat_db
+    await m_cat_db.push(10000001)
+    await m_cat_db.push(10000002)
+    await m_cat_db.push(10000003)
+    await m_cat_db.push(10000005)
+    await m_cat_db.push(10000002)
+    s_all = {'10000001', '10000002', '10000003', '10000005'}
+    assert await m_cat_db.get_all() == s_all
+
+    r = await m_cat_db.pop()
+    assert r in s_all
+
+    message_id = await m_cat_db.pop()
+    while message_id:
+        message_id = await m_cat_db.pop()
+
+    assert await m_cat_db.get_all() == set()
