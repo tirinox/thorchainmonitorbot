d0a8d06719dd6742efde25762aa248f52a3e0733
Tirinox <develoloper@tirinox.ru>
2024-10-03 22:41:05 +0300

    Draw funcs: gradients

--- app/lib/draw_utils.py
+++ app/lib/draw_utils.py
@@ -739,3 +739,85 @@ def draw_text_with_font(text: str, font: ImageFont, text_color=(0, 0, 0, 255), s
               anchor='lt')
 
     return image
+
+
+def linear_gradient(target_image: Image, poly, p1, p2, c1, c2):
+    """
+    Draw polygon with linear gradient from point 1 to point 2 and ranging
+    from color 1 to color 2 on given image
+
+    :param target_image: Image to draw on
+    :param poly: Polygon to draw [(x1, y1), (x2, y2), ...]
+    :param p1: Start point of gradient
+    :param p2: End point of gradient
+    :param c1: Start color of gradient (R, G, B)
+    :param c2: End color of gradient (R, G, B)
+    """
+    # Draw initial polygon, alpha channel only, on an empty canvas of image size
+    ii = Image.new('RGBA', target_image.size, (0, 0, 0, 0))
+    draw = ImageDraw.Draw(ii)
+    draw.polygon(poly, fill=(0, 0, 0, 255), outline=None)
+
+    # Calculate angle between point 1 and 2
+    p1 = np.array(p1)
+    p2 = np.array(p2)
+    angle = np.arctan2(p2[1] - p1[1], p2[0] - p1[0]) / np.pi * 180
+
+    # Rotate and crop shape
+    temp = ii.rotate(angle, expand=True)
+    temp = temp.crop(temp.getbbox())
+    wt, ht = temp.size
+
+    w, h = target_image.size
+    # Create gradient from color 1 to 2 of appropriate size
+    gradient = np.linspace(c1, c2, wt, True).astype(np.uint8)
+    gradient = np.tile(gradient, [2 * h, 1, 1])
+    gradient = Image.fromarray(gradient)
+
+    # Paste gradient on blank canvas of sufficient size
+    temp = Image.new('RGBA', (max(target_image.size[0], gradient.size[0]),
+                              max(target_image.size[1], gradient.size[1])), (0, 0, 0, 0))
+    temp.paste(gradient)
+    gradient = temp
+
+    # Rotate and translate gradient appropriately
+    x = np.sin(angle * np.pi / 180) * ht
+    y = np.cos(angle * np.pi / 180) * ht
+    gradient = gradient.rotate(-angle, center=(0, 0),
+                               translate=(p1[0] + x, p1[1] - y))
+
+    # Paste gradient on temporary image
+    ii.paste(gradient.crop((0, 0, ii.size[0], ii.size[1])), mask=ii)
+
+    # Paste temporary image on actual image
+    target_image.paste(ii, mask=ii)
+
+    return target_image
+
+
+# Draw polygon with radial gradient from point to the polygon border
+# ranging from color 1 to color 2 on given image
+def radial_gradient(i, poly, p, c1, c2):
+    # Draw initial polygon, alpha channel only, on an empty canvas of image size
+    ii = Image.new('RGBA', i.size, (0, 0, 0, 0))
+    draw = ImageDraw.Draw(ii)
+    draw.polygon(poly, fill=(0, 0, 0, 255), outline=None)
+
+    # Use polygon vertex with the highest distance to given point as end of gradient
+    p = np.array(p)
+    max_dist = max([np.linalg.norm(np.array(v) - p) for v in poly])
+
+    # Calculate color values (gradient) for the whole canvas
+    x, y = np.meshgrid(np.arange(i.size[0]), np.arange(i.size[1]))
+    c = np.linalg.norm(np.stack((x, y), axis=2) - p, axis=2) / max_dist
+    c = np.tile(np.expand_dims(c, axis=2), [1, 1, 3])
+    c = (c1 * (1 - c) + c2 * c).astype(np.uint8)
+    c = Image.fromarray(c)
+
+    # Paste gradient on temporary image
+    ii.paste(c, mask=ii)
+
+    # Paste temporary image on actual image
+    i.paste(ii, mask=ii)
+
+    return i
