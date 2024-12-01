81b25785d8f2bac3259229e61703627ffa7c50c3
Tirinox <develoloper@tirinox.ru>
2024-12-01 18:11:24 +0300

    Jinja2 ext of templates

--- renderer/demo/foo.py
+++ renderer/demo/foo.py
@@ -1,5 +1,5 @@
 def demo_foo_parameters():
-    return "foo.html", {
+    return "foo.jinja2", {
         "title": "Demo 2",
         "heading": "Hello 2",
         "message": "This is a demo template."
--- renderer/demo/rune_burn_and_income.py
+++ renderer/demo/rune_burn_and_income.py
@@ -1,4 +1,4 @@
 def demo_rune_burn_and_income_parameters():
-    return "rune_burn_and_income.html", {
+    return "rune_burn_and_income.jinja2", {
         "todo": "params"
     }
