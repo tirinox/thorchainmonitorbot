6c0c4fe9041d34aab935b223154ae3189126b34c
Tirinox <tirinox@gmail.com>
2025-01-26 21:11:49 +0300

    Tool: ensure that all existing pools have their logo locally downloaded

---
+++ app/tools/ensure_downloaded_asset_logo.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+
+import tqdm
+
+from comm.picture.crypto_logo import CryptoLogoDownloader
+from comm.picture.resources import Resources
+from tools.lib.lp_common import LpAppFramework
+
+
+async def do_download_job(app):
+    await app.deps.pool_fetcher.run_once()
+    pools = app.deps.price_holder.pool_names
+    print(pools)
+
+    logo_downloader = CryptoLogoDownloader(Resources().LOGO_BASE)
+    for pool in tqdm.tqdm(pools):
+        await logo_downloader.get_or_download_logo_cached(pool)
+
+    # asset_image = await r.logo_downloader.get_or_download_logo_cached(asset)
+
+
+async def main():
+    app = LpAppFramework(log_level=logging.INFO)
+    async with app(brief=True):
+        await do_download_job(app)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
