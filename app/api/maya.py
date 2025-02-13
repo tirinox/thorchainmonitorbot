a9723bec6b35342096ffaab63952c25cd83a21c6
Tirinox <tirinox@gmail.com>
2025-02-13 20:09:03 +0300

    Small change

--- app/api/maya.py
+++ app/api/maya.py
@@ -2,7 +2,7 @@ from contextlib import suppress
 
 from aiohttp import ClientSession
 
-from lib.constants import thor_to_float
+from lib.constants import thor_to_float, NATIVE_RUNE_SYMBOL
 from lib.logs import WithLogger
 
 MAYA_POOLS_URL = 'https://mayanode.mayachain.info/mayachain/pools'
@@ -18,6 +18,6 @@ class MayaConnector(WithLogger):
             self.logger.info(f'Fetching Maya pool balance from {MAYA_POOLS_URL}')
             async with self.session.get(MAYA_POOLS_URL) as resp:
                 j = await resp.json()
-                rune_pool = next(p for p in j if p['asset'] == 'THOR.RUNE')
+                rune_pool = next(p for p in j if p['asset'] == NATIVE_RUNE_SYMBOL)
                 return thor_to_float(rune_pool['balance_asset'])
         return 0.0
