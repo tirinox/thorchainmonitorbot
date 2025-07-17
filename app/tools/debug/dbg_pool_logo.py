520a743c3ec00e7cdb3a61ac9eb1cc48768f3ba9
Tirinox <tirinox@gmail.com>
2025-07-17 16:06:34 +0300

    dbg_pool_logo page to see if all pool logos are present

---
+++ app/tools/debug/dbg_pool_logo.py
@@ -0,0 +1,27 @@
+import asyncio
+import json
+
+from tools.lib.lp_common import LpAppFramework
+
+DEMO_POOL_LOGO_FILENAME = "./renderer/demo/dbg_pool_logo.json"
+
+
+async def main():
+    app = LpAppFramework()
+    async with app:
+        pools = list(app.deps.price_holder.pool_names)
+        print(pools)
+
+        with open(DEMO_POOL_LOGO_FILENAME, "w") as f:
+            json.dump({
+                "template_name": "dbg_pool_logo.jinja2",
+                "parameters": {
+                    "_width": 1280,
+                    "_height": 920,
+                    "pools": pools,
+                }
+            }, f, indent=2)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
