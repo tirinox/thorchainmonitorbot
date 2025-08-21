b10c36d7124c8bd6e19e8d3d2f824320811fb50c
Tirinox <tirinox@gmail.com>
2025-08-21 16:57:20 +0300

    Renderer: "template not found" gives a list of available

--- app/renderer/demo/__init__.py
+++ app/renderer/demo/__init__.py
@@ -6,9 +6,12 @@ def demo_template_parameters(name: str):
     # path = current file path + name.json
     path = os.path.join(os.path.dirname(__file__), f"{name}.json")
 
-    with open(os.path.join(path)) as f:
-        data = json.load(f)
-        return data['template_name'], data['parameters']
+    try:
+        with open(os.path.join(path)) as f:
+            data = json.load(f)
+            return data['template_name'], data['parameters']
+    except FileNotFoundError:
+        return None, None
 
 
 def available_demo_templates():
