bd70ae4c6e90075ba9030808eb78272c30a5a905
Tirinox <tirinox@gmail.com>
2025-12-04 15:42:20 +0300

    Mimir: auto add known assets to mimir dict

--- app/tools/mimir_add_word.py
+++ app/tools/mimir_add_word.py
@@ -7,19 +7,22 @@ from tools.lib.lp_common import LpAppFramework
 async def main():
     lp_app = LpAppFramework()
     async with lp_app:
+        pools = await lp_app.deps.pool_cache.get_pools()
+
         mimir_rules = MimirNameRules()
         mimir_rules.load(MIMIR_DICT_FILENAME)
+        mimir_rules.update_asset_names(pools)
 
         new_words = input('Enter new words to add to mimir naming rules (space separated): ').split()
         if not new_words:
             print('No words to add')
             return
 
-        old_words = set(mimir_rules.dict_word_sorted)
+        old_words = set(mimir_rules.known_words)
 
         mimir_rules.add_words(new_words)
 
-        new_words = set(mimir_rules.dict_word_sorted) - old_words
+        new_words = set(mimir_rules.known_words) - old_words
         print(f'Added words: {new_words}')
 
         if input('Save changes to mimir naming rules? [y/N]: ').lower() == 'y':
