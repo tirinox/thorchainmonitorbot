2dbceb50b263f34716e7f3af63910c3a959670e7
Tirinox <tirinox@gmail.com>
2026-02-06 15:28:37 +0300

    Flagship usage: delegates flow

--- app/lib/delegates.py
+++ app/lib/delegates.py
@@ -2,6 +2,8 @@ import logging
 import time
 from abc import ABC, abstractmethod
 
+from lib.flagship import Flagship
+
 
 class INotified(ABC):
     @abstractmethod
@@ -38,17 +40,36 @@ class WithDelegates:
         summary = {}
 
         for delegate in self.delegates:
+            if not await self.is_passage_allowed(delegate):
+                logging.warning(f"Passage not allowed to delegate {delegate}")
+                continue
+
             delegate: INotified
             t0 = time.monotonic()
+
             try:
                 await delegate.on_data(sender, data)
             except Exception as e:
                 logging.exception(f"{e!r}")
-                # logging.error(
-                #     "Call stack at pass_data_to_listeners catch site:\n%s",
-                #     "".join(traceback.format_stack())
-                # )
+
             t1 = time.monotonic()
             summary[str(delegate)] = t1 - t0
 
         return summary
+
+    @property
+    def my_class_name(self):
+        return self.__class__.__name__
+
+    async def is_passage_allowed(self, sender=None):
+        if flagship := self.maybe_get_flagship():
+            flag_name = f"pass:{self.my_class_name}:{sender.__class__.__name__}"
+            if not await flagship.is_flag_set(flag_name):
+                return False
+        return True
+
+    def maybe_get_flagship(self) -> Flagship | None:
+        if hasattr(self, 'deps'):
+            d = getattr(self, 'deps')
+            return d.flagship
+        return None
